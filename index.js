d37fcf76e1ab46a352193624b8bfdadb493dc8b9
1medi <144403286+1medi@users.noreply.github.com>
2025-03-23 18:37:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import pg from "pg";
 import multer from "multer";
 import axios from "axios";
 import dotenv from "dotenv";
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -38,28 +38,30 @@ function findStoreInRecommended(storeId) {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const allRecommendedStores = JSON.parse(
-    fs.readFileSync(path.join(__dirname, 'recommended.json'), 'utf8')
+    fs.readFileSync(path.join(__dirname, "recommended.json"), "utf8")
   );
-  return allRecommendedStores.find(store => store.id === storeId);
+  return allRecommendedStores.find((store) => store.id === storeId);
 }
-
 
 // Helper function to fetch place details
 async function fetchPlaceDetails(placeId) {
   const placeDetailsUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
   const placeDetailsResponse = await axios.get(placeDetailsUrl);
-  const openingHours = placeDetailsResponse.data.result.opening_hours?.weekday_text; // Extract opening hours
+  const openingHours =
+    placeDetailsResponse.data.result.opening_hours?.weekday_text; // Extract opening hours
   return {
-    openingHours: Array.isArray(openingHours) ? openingHours : [openingHours]
+    openingHours: Array.isArray(openingHours) ? openingHours : [openingHours],
   };
 }
 
 async function getPlaceIdFromDatabase(storeId) {
   try {
-    const result = await db.query("SELECT place_id FROM stores WHERE id = $1", [storeId]);
+    const result = await db.query("SELECT place_id FROM stores WHERE id = $1", [
+      storeId,
+    ]);
     return result.rows.length > 0 ? result.rows[0].place_id : null; // Return the Place ID or null if not found
   } catch (error) {
-    console.error('Error fetching Place ID:', error);
+    console.error("Error fetching Place ID:", error);
     return null; // Handle error gracefully
   }
 }
@@ -71,7 +73,9 @@ app.get("/", async (req, res) => {
     const stores = result.rows;
     const geocodePromises = stores.map(async (store) => {
       const address = store.address;
-      const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+      const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+        address
+      )}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
 
       try {
         const geocodeResponse = await axios.get(geocodeUrl);
@@ -82,7 +86,7 @@ app.get("/", async (req, res) => {
           store.longitude = location.lng;
         }
       } catch (geocodeError) {
-        console.error('Geocoding error:', geocodeError);
+        console.error("Geocoding error:", geocodeError);
       }
     });
 
@@ -90,11 +94,16 @@ app.get("/", async (req, res) => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     const allRecommendedStores = JSON.parse(
-      fs.readFileSync(path.join(__dirname, 'recommended.json'), 'utf8')
-    );    
+      fs.readFileSync(path.join(__dirname, "recommended.json"), "utf8")
+    );
     const recommendedStores = getRandomStores(allRecommendedStores, 3);
 
-    res.render("index", { stores, googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY, recommendedStores, allRecommendedStores });
+    res.render("index", {
+      stores,
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      recommendedStores,
+      allRecommendedStores,
+    });
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -117,7 +126,9 @@ app.get("/store/:id", async (req, res) => {
   let placeId;
 
   try {
-    const dbResult = await db.query("SELECT * FROM stores WHERE id = $1", [storeId]);
+    const dbResult = await db.query("SELECT * FROM stores WHERE id = $1", [
+      storeId,
+    ]);
 
     if (dbResult.rows.length > 0) {
       store = dbResult.rows[0];
@@ -149,19 +160,24 @@ app.get("/store/:id", async (req, res) => {
   }
 });
 
-
-app.post('/add-store', upload.single('image'), async (req, res) => {
-  const { name, address, contact_info, parking } = req.body; 
+app.post("/add-store", upload.single("image"), async (req, res) => {
+  const { name, address, contact_info, parking } = req.body;
   const image = req.file.buffer;
-  const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+  const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+    address
+  )}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
 
   try {
     const geocodeResponse = await axios.get(geocodeUrl);
     const location = geocodeResponse.data.results[0]?.geometry?.location;
-    const placeId = geocodeResponse.data.results[0]?.place_id; 
+    const placeId = geocodeResponse.data.results[0]?.place_id;
 
     if (!location || !placeId) {
-      return res.status(400).send('Unable to find coordinates or place ID for the provided address.');
+      return res
+        .status(400)
+        .send(
+          "Unable to find coordinates or place ID for the provided address."
+        );
     }
 
     const latitude = location.lat;
@@ -171,17 +187,27 @@ app.post('/add-store', upload.single('image'), async (req, res) => {
     const { openingHours, closingHours } = await fetchPlaceDetails(placeId);
 
     // Insert into database
-    const query = 'INSERT INTO stores(name, address, contact_info, image, latitude, longitude, parking, place_id, opening_hours) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
-    const values = [name, address, contact_info, image, latitude, longitude, parking, placeId, openingHours];
+    const query =
+      "INSERT INTO stores(name, address, contact_info, image, latitude, longitude, parking, place_id, opening_hours) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+    const values = [
+      name,
+      address,
+      contact_info,
+      image,
+      latitude,
+      longitude,
+      parking,
+      placeId,
+      openingHours,
+    ];
     await db.query(query, values);
 
-    res.redirect('/');
+    res.redirect("/");
   } catch (err) {
     console.error(err); // Log the error details
-    res.status(500).send('Error inserting store or geocoding address');
+    res.status(500).send("Error inserting store or geocoding address");
   }
 });
-
 
 // Add a recommended store
 app.post("/store/add", async (req, res) => {
@@ -191,24 +217,40 @@ app.post("/store/add", async (req, res) => {
   console.log("Inserting into database with Place ID:", place_id); // Log the place_id being inserted
 
   try {
-    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
     const geocodeResponse = await axios.get(geocodeUrl);
     const location = geocodeResponse.data.results[0]?.geometry?.location;
-    if (!location) return res.status(400).json({ success: false, message: "Could not geocode address" });
+    if (!location)
+      return res
+        .status(400)
+        .json({ success: false, message: "Could not geocode address" });
 
-const result = await db.query(
-  "INSERT INTO stores(name, address, contact_info, image, latitude, longitude, place_id) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id",
-  [name, address, 'Added from recommendations', image, location.lat, location.lng, place_id]
+    const result = await db.query(
+      "INSERT INTO stores(name, address, contact_info, image, latitude, longitude, place_id) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+      [
+        name,
+        address,
+        "Added from recommendations",
+        image,
+        location.lat,
+        location.lng,
+        place_id,
+      ]
     );
 
-    res.json({ success: true, store: { id: result.rows[0].id, name, address, image } });
+    res.json({
+      success: true,
+      store: { id: result.rows[0].id, name, address, image },
+    });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
 });
 
-app.post('/add-review', upload.single('image'), async (req, res) => {
-  console.log('Request Body:', req.body); // Log the entire request body
+app.post("/add-review", upload.single("image"), async (req, res) => {
+  console.log("Request Body:", req.body); // Log the entire request body
 
   const { storeId, rating, comment, reviewerName } = req.body;
   let imagePath = null;
@@ -218,15 +260,16 @@ app.post('/add-review', upload.single('image'), async (req, res) => {
   }
 
   if (!storeId || !rating || !comment || !reviewerName) {
-    return res.status(400).send('All fields are required.');
+    return res.status(400).send("All fields are required.");
   }
 
-  const query = 'INSERT INTO reviews(store_id, rating, comment, reviewer_name, image) VALUES($1, $2, $3, $4, $5)';
+  const query =
+    "INSERT INTO reviews(store_id, rating, comment, reviewer_name, image) VALUES($1, $2, $3, $4, $5)";
   const values = [storeId, rating, comment, reviewerName, imagePath];
 
   try {
     await db.query(query, values); // Execute the review insertion
-    res.json({ success: true, message: 'Review added successfully.' });
+    res.json({ success: true, message: "Review added successfully." });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
@@ -238,21 +281,34 @@ app.post("/store/add", async (req, res) => {
   console.log("Inserting into database with Place ID:", place_id); // Log the place_id being inserted
 
   try {
-    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
     const geocodeResponse = await axios.get(geocodeUrl);
     const location = geocodeResponse.data.results[0]?.geometry?.location;
-    if (!location) return res.status(400).json({ success: false, message: "Could not geocode address" });
+    if (!location)
+      return res
+        .status(400)
+        .json({ success: false, message: "Could not geocode address" });
 
     const result = await db.query(
       "INSERT INTO stores(name, address, contact_info, image, latitude, longitude, place_id) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id",
-      [name, address, 'Added from recommendations', image, location.lat, location.lng, place_id]
+      [
+        name,
+        address,
+        "Added from recommendations",
+        image,
+        location.lat,
+        location.lng,
+        place_id,
+      ]
     );
 
     const newStore = {
       id: result.insertId,
       name,
       address,
-      image
+      image,
     };
 
     res.json({ success: true, store: newStore });
